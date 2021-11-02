683fd4107437fdc5936418c90bea77af625f37ba
asncn <asncn@mail.ru>
2021-11-02 21:59:30 +0300

    fixed local

--- src/button/index.js
+++ src/button/index.js
@@ -1,12 +1,24 @@
 import { createElement } from '../createElement';
 
 export default class Btn {
-    constructor(content, className, parent, position, classColor, classSize) {
+    constructor(content, className, parent, position) {
         this.content = content;
         this.className = className;
         this.parent = parent;
         this.position = position;
-        this.classColor = classColor;
-        this.classSize = classSize;
+    };
+    // создание самого элемента 
+    //методы добавления стилей 
+    //методы добваления классов
+
+    $createElement() {
+        return createElement("button", this.content, this.className, this.parent, this.position)
+
+    };
+
+    $addClasses(el, arrayClasses) {
+        arrayClasses.map((className) => {
+            el.classList.add(className)
+        })
     }
-}
+};
--- src/drawTodos.js
+++ src/drawTodos.js
@@ -1,30 +1,26 @@
 import { createElement } from './createElement';
-import { Btn } from './button';
+import Btn from './button';
 
 export function drawTodos(todo, ol, delete_btns) {
     todo.map((item, index) => {
         let li = createElement("li", item, "li", ol, "beforeend");
         li.style.marginTop = "1em";
         li.setAttribute("key", index);
-        let btn_delete = createElement(
-            "button",
-            "",
-            "button",
-            li,
-            "beforeend"
-        );
+        let btn = new Btn("", "button", li, "beforeend");
+        let btn_delete = btn.$createElement();
+
         btn_delete.classList.add("is-danger");
         btn_delete.classList.add("is-small");
         btn_delete.style.marginLeft = "3em";
         const bucket = createElement("span", "", "mdi-bucket-outline", btn_delete, "beforeend");
-        bucket.classList.add('mdi')
+        bucket.classList.add('mdi');
         delete_btns = [];
         delete_btns.push(btn_delete);
         delete_btns.map((btn) => {
             btn.onclick = () => {
                 const key = btn.parentNode.getAttribute("key");
-                localStorage.setItem("todo", JSON.stringify(todo))
                 todo.splice(key, 1);
+                localStorage.setItem("todo", JSON.stringify(todo));
                 btn.parentNode.remove();
             };
         });
